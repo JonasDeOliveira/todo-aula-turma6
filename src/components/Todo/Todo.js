c04f4598cfa57fd299bbf1ff350445047ac73bc6
JonasDeOliveira <jonas.doliver@gmail.com>
2021-10-26 17:53:09 -0300

    post

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import TodoForm from './Form/TodoForm'
 import TodoList from './List/TodoList'
 
 function Todo(props) {
+
+    const URL = 'http://localhost:3001/todo'
+
+    useEffect(() => {
+        registerTask('12121')
+    }, [])
+
+    const registerTask  = descParam => {
+        axios.post(`${URL}`, { description: descParam, done: false})
+        .then((response)=> {
+            console.log(response)
+        })
+    }
 
     return(
         <>
